0e82b62fcab42358b45acf12e718f1887ac96326
Mbrain <ennocentcip@gmail.com>
2024-03-04 15:57:34 +0100

    modify modal app

--- modal popup/script.js
+++ modal popup/script.js
@@ -2,10 +2,20 @@ const btn = document.querySelector('.modal-btn');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.close');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
   modal.classList.add('show-modal');
 });
 
 closeBtn.addEventListener('click', function () {
-  modal.classList.remove('show-modal');
+  // modal.classList.remove('show-modal');
+  closeModal();
 });
+modal.addEventListener('click', function () {
+  // modal.classList.remove('show-modal');
+  closeModal();
+});
+
+function closeModal() {
+  modal.classList.remove('show-modal');
+}
